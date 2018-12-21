8ab895e9534c401f09bf37b3611ec17ca004f888
Alvin <alvinchun911@gmail.com>
2018-12-20 19:10:14 -0500

    New colors refresh button

--- RGBgame.js
+++ RGBgame.js
@@ -14,6 +14,36 @@ let pickedColor = pickColor();
 let colorDisplay = document.querySelector("#color-display");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
+let resetButton = document.querySelector("#reset");
+let easyBtn = document.querySelector("#easyBtn");
+let hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function() {
+  hardBtn.classList.remove("selected");
+  easyBtn.classList.add("selected");
+  colors = generateRandomColors(3);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+});
+
+hardBtn.addEventListener("click", function() {
+  easyBtn.classList.remove("selected");
+  hardBtn.classList.add("selected");
+});
+
+resetButton.addEventListener("click", function() {
+  //generate all new colors
+  colors = generateRandomColors(6);
+  //pick a new random color from array
+  pickedColor = pickColor();
+  // change colors of squares
+  colorDisplay.textContent = pickedColor;
+  //change colorDisplay to match picked color
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i];
+  }
+  h1.style.background = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -28,6 +58,7 @@ for (let i = 0; i < squares.length; i++) {
     for (let i = 0; i < squares.length; i++) {
       if (clickedColor === pickedColor) {
         messageDisplay.textContent = "Correct!";
+        resetButton.textContent = "Play Again?";
         changeColors(clickedColor);
         h1.style.background = clickedColor;
       } else {
